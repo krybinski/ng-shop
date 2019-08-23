cb572de6e881ff2e5991be1a9ccbb7e1177dc2f6
Kamil Rybiński <kamilryba19@gmail.com>
2019-08-23 21:30:37 +0200

    Added login with Google using Firebase

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'app/auth.service';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
-  ngOnInit() {
+  logout() {
+    this.auth.logout();
   }
 
 }
